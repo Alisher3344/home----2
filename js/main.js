419aacd23555dc8e00b557ee92442311a3a21eee
Alisher3344 <alisher3344ali@gmail.com>
2024-07-17 14:04:59 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,140 @@
+// // Boolean1
+// let a = 14;
+// let b = a % 2 == 0;
+// console.log(b);
+
+// Boolean2
+
+// // Boolean3
+// let a = 5;
+// let b = 9;
+// let c = 15;
+// let d = a < b <+ c;
+// console.log(d);
+
+// // Boolean4
+// let a = 5;
+// let b = 3;
+// let c = a % 2 == 1 && b % 2 == 1;
+// console.log(c);
+
+// // Boolean5
+// let a = 3;
+// let b = 2;
+// let c = a % 2 == 0 || b % 2 == 0;
+// console.log(c);
+
+// // Boolean6
+// let a = 2;
+// let b = 7;
+// let c = a % 2 == 0 || b % 2 == 0;
+// console.log(c);
+
+// // Boolean7
+// let a = -2;
+// let b = -7;
+// let c = -3;
+// let d = a < 0 && b < 0 && c < 0;
+// console.log(d);
+
+// // Boolean8
+// let a = -2;
+// let b = 7;
+// let c = 3;
+// let d = a < 0 || b < 0 || c < 0;
+// console.log(d);
+
+// Boolean9
+// let a = 2;
+// let b = 7;
+// let c = 3;
+// let d = a > 0 || b < 0 || c < 0;
+// console.log(d);
+
+// // Boolean10
+// let a = 10;
+// let b = a > 9 && a % 2 == 0;
+// console.log(b);
+
+// // Boolean11
+// let a = 101;
+// let b = a > 99 && a % 2 == 1;
+// console.log(b);
+
+// Boolean12
+
+
+// // ifoda1
+// let A = 1;
+// let B = 4;
+// let c = (B / 2) * (Math.pow(A, 2) + Math.pow(B, 2));
+// console.log(c);
+
+// // ifoda2
+// let a = 3;
+// let b = 2;
+// let c = 4;
+// let d = 1;
+// let e = Math.pow((a + b) / (c + d), 2) + Math.pow(b, 2);
+// console.log(e);
+
+// // ifoda3
+// let A = 2;
+// let C = 3;
+// let D = 5;
+// let Y = 2 * (Math.pow(A, D) + (4 * Math.pow(C, 2)) / 3);
+// console.log(Y);
+
+// // ifoda4
+// let a = 2;
+// let b = 20;
+// let c = 4;
+// let d = 3;
+// let Y = (1 / Math.pow(a, 2)) * Math.pow(b / 10, 3) * Math.pow(c + d, 2);
+// console.log(Y);
+
+// // ifoda5
+// let a = 4;
+// let b = 2;
+// let c = 10;
+// let d = 6;
+// let y = a / b;
+// let x = c / 1 - d;
+// let Y = Math.pow(y - 1 / x, 2);
+// console.log(Y);
+
+// // ifoda6
+// let a = 4;
+// let b = 2;
+// let c = 8;
+// let d = 3;
+// let y = c + d / (a + c);
+// let Y = y / (a + b);
+// console.log(Y);
+
+// // ifoda7
+// let a = 4;
+// let b = 2;
+// let c = 8;
+// let y = Math.pow(a + b + c, 2);
+// let x = Math.pow(a - b - c, 2);
+// let Y = Math.sqrt(y - x);
+// console.log(Y);
+
+// // ifoda8
+// let a = 4;
+// let b = 2;
+// let c = 8;
+// let y = Math.pow(a, 2) + Math.pow(b, 2) + Math.pow(c, 2);
+// let Y = y / (a * b * c);
+// console.log(Y);
+
+// // ifoda9
+// let a = 4;
+// let b = 2;
+// let c = 8;
+// let d = 3;
+// let y = Math.pow(d, 3);
+// let x = Math.pow(c, 2);
+// let Y = a * (y / 3) + b * (x / 2);
+// console.log(Y);
